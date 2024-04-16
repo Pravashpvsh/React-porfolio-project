bb84f021fd72786e2fda600af6e351254bf0817f
Pravash Pokhrel <pokhrelpravash90@gmail.com>
2024-04-16 21:24:14 +0545

    add

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+      <h1>Portfolio</h1>
+      <nav>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Work</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
